553f72afb98495e4cda8152a98f7d12e2497bbdf
Rajvir Singh <rajvirx.ai@gmail.com>
2026-01-19 22:30:51 +0530

    Day 8

---
+++ grade_system.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter marks (0-100): "))
+
+if marks >= 90:
+    print("Grade: A")
+
+elif marks >= 75:
+    print("Grade: B")
+
+elif marks >= 50:
+    print("Grade: C")
+
+else:
+    print("Grade: Fail")
